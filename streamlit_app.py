15eccb8d50589cf4b11e532a20519490c736bf09
sharma764 <121413640+sharma764@users.noreply.github.com>
2022-12-30 22:47:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,4 @@ streamlit.write('The user entered', fruit_choice)
 import requests
 fruityvice_response = response.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 streamlit.dataframe(fruityvice_normalized)
+streamlit.header("Fruityvice Fruit Advice!")
