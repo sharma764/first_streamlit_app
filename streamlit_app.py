0b02d09cd2b0c24f44caec24626591f5d1d0c897
sharma764 <121413640+sharma764@users.noreply.github.com>
2023-01-01 16:29:18 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,8 +25,8 @@ streamlit.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
-fruit_choice = streamlit.text_input('What fruit would you like information about?'about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#streamlit.write('The user entered', fruit_choice)
 if not fruit_choice:
 streamlit.error("Please select a fruit to get information.")
 else:
@@ -46,7 +46,7 @@ streamlit.error()
 
 
 # don't run anything past here while we troubleshoot
-#streamlit.stop()
+streamlit.stop()
 #import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
